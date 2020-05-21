d354514011fb623867b6b26b115f4f3a643b5144
EliseevVadim <55801514+EliseevVadim@users.noreply.github.com>
2020-05-21 12:27:23 +0300

    third task, list decision

---
+++ 10.3LD/10.3LD/Graph.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10._3LD
+{
+    class Graph
+    {
+        Dictionary<int, List<int>> graph = null;
+        public void InitGraphStructure(Dictionary<int, List<int>> dict)
+        {
+            graph = dict;
+        }
+        public Stack<int> BFS(int start, int end, int verticlecount)
+        {
+            Queue<int> queue = new Queue<int>();
+            int[] vpath = new int[verticlecount];
+            int[] checkdv = new int[verticlecount];
+            queue.Enqueue(start);
+            checkdv[start] = 1;
+            while (queue.Count > 0)
+            {
+                int i = queue.Dequeue();
+                for (int j = 0; j < verticlecount; j++)
+                {
+                    if (graph[i].Contains(j) && checkdv[j] == 0)
+                    {
+                        checkdv[j] = 1;
+                        queue.Enqueue(j);
+                        vpath[j] = i;
+                        if (j == end)
+                        {
+                            return GetResult(vpath, start, end);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        public Stack<int> GetResult(int[] mas, int start, int end)
+        {
+            int pos = end;
+            Stack<int> path = new Stack<int>();
+            path.Push(pos);
+            while (pos != start)
+            {
+                pos = mas[pos];
+                path.Push(pos);
+            }
+            return path;
+        }
+        public Stack<int> DFS(int start, int end, int verticlecount)
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] vpath = new int[verticlecount];
+            int[] checkdv = new int[verticlecount];
+            stack.Push(start);
+            checkdv[start] = 1;
+            while (stack.Count > 0)
+            {
+                int i = stack.Pop();
+                for (int j = 0; j < verticlecount; j++)
+                {
+                    if (graph[i].Contains(j) && checkdv[j] == 0)
+                    {
+                        checkdv[j] = 1;
+                        stack.Push(j);
+                        vpath[j] = i;
+                        if (j == end)
+                        {
+                            return GetResult(vpath, start, end);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ 10.3LD/10.3LD/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+namespace _10._3LD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, List<int>> ways = new Dictionary<int, List<int>>();
+            ways.Add(1, new List<int> { 2, 3 });
+            ways.Add(2, new List<int> { 1, 7, 6 });
+            ways.Add(3, new List<int> { 1, 6, 8 });
+            ways.Add(4, new List<int> { 3, 5 });
+            ways.Add(5, new List<int> { 4, 6 });
+            ways.Add(6, new List<int> { 2, 3, 5 });
+            ways.Add(7, new List<int> { 2, 8 });
+            ways.Add(8, new List<int> { 7, 3 });
+            Graph graph = new Graph();
+            graph.InitGraphStructure(ways);
+            Console.WriteLine("Введите начальную вершину: ");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите конечную вершину: ");
+            int end = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Stack<int> BFS = graph.BFS(start, end, 9);
+            int c = 0;
+            Console.WriteLine("BFS: ");
+            try
+            {
+                while (BFS.Count != 0)
+                {
+                    if (c == 0)
+                    {
+                        Console.Write((BFS.Pop()));
+                    }
+                    else
+                    {
+                        Console.Write("->" + ( BFS.Pop()));
+                    }
+                    c++;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Пути нет");
+            }
+            Console.WriteLine();
+            Console.WriteLine("DFS: ");
+            Stack<int> DFS = graph.DFS(start, end, 9);
+            int n = 0;
+            try
+            {
+                while (DFS.Count != 0)
+                {
+                    if (n == 0)
+                    {
+                        Console.Write((DFS.Pop()));
+                    }
+                    else
+                    {
+                        Console.Write("->" + (DFS.Pop()));
+                    }
+                    n++;
+                }
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Пути нет");
+            }
+        }
+    }
+}
